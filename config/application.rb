85fe8b767f6b82cdfda3d4e533023f47955e3c4e
Radhika S. Tiwari <radhikatiwari1708@gmail.com>
2015-05-16 11:09:28 +0530

    carrier wave changes

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Eventino
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
     config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJR27QDM6NHNW7ZGQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['eventinoapp']
+    config.fog_directory     =  ENV['S3_Bucket']
   end
 end
